f4d527b734c0e1e821126e3a1cf956fa32e46697
Topsurpass <temitopeabiodun685@gmail.com>
2025-05-14 23:15:28 +0100

    adjusted the access and refreshtoken expiry time

--- intranextserver/settings.py
+++ intranextserver/settings.py
@@ -188,8 +188,8 @@ AUTH_USER_MODEL = 'users.User'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
-    'REFRESH_TOKEN_LIFETIME': timedelta(minutes=10),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
     'AUTH_HEADER_TYPES': ('Bearer',),
